8cac737ffb02d85be4ff29cefb631a084f552733
mmarolin <matija.marolin11@gmail.com>
2022-04-26 21:54:33 +0200

    adding reset functionality

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
 
@@ -61,6 +68,7 @@ class _MyAppState extends State<MyApp> {
     var condition = _questionIndex < _questions.length;
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
@@ -72,7 +80,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,16 +2,37 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = ' ';
+    if (resultScore == 3) {
+      resultText = 'Good job! You answered all questions correctly.';
+    } else {
+      resultText = 'Try again! You got only ${resultScore}/3 questions right';
+    }
+
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'You got ${resultScore}/3 answers right! Good job!',
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        child: Column(children: <Widget>[
+      Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
-    );
+      OutlineButton(
+        child: Text(
+          'Restart Quiz!',
+        ),
+        textColor: Colors.blue,
+        onPressed: resetHandler,
+      ),
+    ]));
   }
 }
